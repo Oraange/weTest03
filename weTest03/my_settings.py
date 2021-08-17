f10ad7a45ed4f5a912ef2af8628103960e96167e
Chi heon Song <hrpp1353@gmail.com>
2021-08-17 16:30:26 +0900

    Add: First commit

---
+++ weTest03/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weTest03',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'dfjlmglrkltl4lklksel3jljlk13j5ms5jlvxjhakerk'
